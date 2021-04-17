6341aa2110f7dc8d30f90111e41ac94ed2c1c17c
Adr205 <A01242469@itesm.mx>
2021-04-17 11:21:39 -0600

    Lista fuentes

---
+++ Maysuculas/FuentesTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  FuentesTableViewCell.swift
+//  Mayusculas
+//
+//  Created by Javier Adrian Villa Leon on 17/04/21.
+//
+
+import UIKit
+
+class FuentesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lNombre: UILabel!
+    @IBOutlet weak var lAyuda: UILabel!
+    @IBOutlet weak var lLink: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
